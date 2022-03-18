5ede70aae02d679ee7e543634da238c03bdbe05d
Reht Cavazos <reht.dev@gmail.com>
2022-03-17 21:35:28 -0500

    initial file structure, css, and file connectivity.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+
+import Header from './components/Header/Header'
+import Navigation from './components/Navigation/Navigation'
+import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
+import Container from './components/Container/Container'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"><img src={logo} className="App-logo" alt="logo" /></header>
+      <Header />
+      <Navigation />
+      <About />
+      <Portfolio />
+      <Container />
+      <Contact />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div>
+      CONTAINER
+    </div>
+  )
+}
+
+export default Container
